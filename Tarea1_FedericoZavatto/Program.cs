e427aa18a0bd6104bd2fc572ed23a4ed13ff41b6
FedericoNZ <federicozavatto@gmail.com>
2022-05-12 16:22:37 -0300

    first commit

---
+++ Tarea1_FedericoZavatto/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string respuesta = "S";
+
+while (respuesta == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su nombre:");
+    string nombreUser = Console.ReadLine();
+
+    Console.WriteLine($"¡Hola {nombreUser}!");
+    Console.WriteLine("¿Desea continuar? (S/N)");
+    respuesta = Console.ReadLine().ToUpper();
+    switch (respuesta)
+    {
+        case "N":
+            Console.WriteLine("Programa finalizado correctamente.");
+            break;
+        default:
+            Console.WriteLine("Opción no válida.");
+            break;
+    }
+}
